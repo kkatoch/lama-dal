d14d9a719545a0cc29f03b28a3af5756befeaef4
Mahendra Katoch <kmkatoch@gmail.com>
2019-10-17 21:14:16 +0100

    Init

---
+++ src/main/java/com/lama/dal/DalApplication.java
@@ -0,0 +1,13 @@
+package com.lama.dal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalApplication.class, args);
+	}
+
+}
