15b9145307f99f9c1d1d537e328b4f9e3bbe75d7
Mahendra Katoch <kmkatoch@gmail.com>
2019-10-24 01:20:26 +0100

    Added CORS for www.

--- src/main/java/com/lama/dal/WebConfig.java
+++ src/main/java/com/lama/dal/WebConfig.java
@@ -13,7 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000","https://www.beta.lamaart.co.uk",
                         "https://www.lamaart.co.uk","https://www.beta.lamaart.uk",
                         "https://www.lamaart.uk","https://www.beta.lamaart.co",
-                        "https://www.lamaart.co")
+                        "https://www.lamaart.co","https://beta.lamaart.co.uk",
+                        "https://lamaart.co.uk","https://beta.lamaart.uk",
+                        "https://lamaart.uk","https://beta.lamaart.co",
+                        "https://lamaart.co")
                 .allowedMethods("GET");
     }
 }
